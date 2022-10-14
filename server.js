282e83bb1bab9c345e8495cb26365e4f6ede8e8c
cakedel <coalgreyy9@gmail.com>
2022-10-14 17:18:55 +0900

    commit

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(express.static(path.join(__dirname, "./build")));
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
 });
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("./build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../kolonindustries", "./build", "index.html"));
+  });
+}
 
 app.get("*", function (req, res) {
   res.header("Access-Control-Allow-origin", "*");
